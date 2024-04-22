f3aaea4023f86306f38ff11f6086b237962ed5b0
NeilMagon <neil.magon97@gmail.com>
2024-04-22 15:17:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+	import PageHeader from './components/PageHeader.vue';
+    import PageMain from './components/PageMain.vue';
+	import PageFooter from './components/PageFooter.vue';
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+@use './style/generic' as *;
+</style>
