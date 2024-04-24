4551f2039e8e5ef231244dfda63faf5b84294150
NeilMagon <neil.magon97@gmail.com>
2024-04-24 12:39:52 +0200

    feat: finish first part of FooterPartners

--- src/components/FooterPartners.vue
+++ src/components/FooterPartners.vue
@@ -11,6 +11,14 @@
                 <div class="text-white">
                     <h1>Subscribe Now to Our Newsletter !</h1>
                 </div>
+                <div class="input-group position-relative w-40">
+                    <input type="email" class="form-control rounded-5" placeholder="Email Address..." aria-label="Indirizzo email" aria-describedby="basic-addon2">
+                    <div class="h-100 position-absolute end-0 ">
+                        <button class="h-100 ms-circle-btn text-white btn rounded-circle px-4" type="button" id="basic-addon2">
+                            <i class="fa-solid fa-paper-plane"></i>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -25,5 +33,12 @@
         h1{
             font-weight: bold;
         }
+        .w-40{
+            width: 40%;
+            .ms-circle-btn{
+                background-color: $brand-secondary;
+            }
+        }
+
     }
 </style>
